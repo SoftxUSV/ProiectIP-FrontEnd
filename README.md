2997ed355b19407aeb5225efde229d9e23bd294a
Guliciuc Denis <guliciuc.denis2003@gmail.com>
2024-11-21 17:13:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ProiectIP-FrontEnd TEST123
---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained" color="primary">
+        Hello Material-UI
+      </Button>
+    </div>
+  );
+}
+
+export default App;
